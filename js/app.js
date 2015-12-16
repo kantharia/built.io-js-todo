d5caf7e180201b697100597c09ddc05e7adb90ab
Chetan Kantharia <chetan.kantharia@raweng.com>
2015-12-16 16:04:53 +0530

    fixed user validation on SignInController

--- js/app.js
+++ js/app.js
@@ -406,7 +406,7 @@ angular.module('todoApp',['ngRoute'])
   })
   .controller('SignInController', function($scope, $location, $rootScope) {
       /* Redirect to `todo` route when user is present */
-      if ($scope.user) {
+      if ($scope.user || $rootScope.currentUser) {
           return $location.path('/todo');
       }
 
@@ -577,5 +577,5 @@ angular.module('todoApp',['ngRoute'])
 
 // Safely apply changes
 function $sa(scope, fn) {
-  (scope.$$phase || scope.$root.$$phase) ? fn() : scope.$apply(fn);
+  (scope.$$phase || scope.$root && scope.$root.$$phase) ? fn() : scope.$apply(fn);
 }
